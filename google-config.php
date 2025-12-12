dec27c5da848a942b0a9bfe577d84aa1a657d311
clark soraino <105158448+riven-sudo@users.noreply.github.com>
2025-12-12 14:58:54 +0800

    Update google-config.php

--- google-config.php
+++ google-config.php
@@ -4,6 +4,7 @@ require_once 'vendor/autoload.php';
 $client = new Google_Client();
 $client->setClientId("859306833735-qlj4iolrfnkhvihpuc856njmej0oivao.apps.googleusercontent.com");
 $client->setClientSecret("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");
-$client->setRedirectUri("https://a85282f07a4180.lhr.life/blackstar/google-callback.php"); // change to your domain
+$client->setRedirectUri("https://test-1-gen7.onrender.com/google-callback.php"); // change to your domain
 $client->addScope("email");
 $client->addScope("profile");
+
